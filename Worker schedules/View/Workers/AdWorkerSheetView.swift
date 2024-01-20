b2a8b8d6a2b1001071f60a4fc012932c25e4bb67
KHVAN DMITRIY <khvandima@gmail.com>
2024-01-20 15:57:09 +0900

    add editWorkerView file

---
+++ Worker schedules/View/Workers/AdWorkerSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  AdWorkerSheetView.swift
+//  Worker schedules
+//
+//  Created by KHVAN DMITRIY on 1/19/24.
+//
+
+import SwiftUI
+
+struct AdWorkerSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdWorkerSheetView()
+}
---
+++ Worker schedules/View/Workers/EditWorkerView.swift
@@ -0,0 +1,18 @@
+//
+//  EditWorkerView.swift
+//  Worker schedules
+//
+//  Created by KHVAN DMITRIY on 1/20/24.
+//
+
+import SwiftUI
+
+struct EditWorkerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditWorkerView()
+}
